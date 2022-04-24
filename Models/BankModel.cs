3c39bd84bbc22a91a8b7423b3b72664ab88e0dff
ALHayden-source <78003163+ALHayden-source@users.noreply.github.com>
2022-04-24 09:36:45 -0500

    Add files via upload

---
+++ Models/BankModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Blazor_Top_Menu.Models
+{
+    public class BankModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
